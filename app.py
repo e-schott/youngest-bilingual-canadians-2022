b3ef608503f80e0487514b5f860b5751a07e4e9c
e-schott <e-schott@users.noreply.github.com>
2022-01-09 18:04:47 -0500

    fixed typo

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ server = app.server
 results = pd.read_csv('assets/bilingual_results_with_region_codes.tsv', sep='\t')
 geo_table = gpd.read_file('assets/recombined_shape_files.zip')
 columns = ['Percent_age_0_to_4', 'Percent_age_5_to_9', 'Percent_age_0_to_9']
-column_labels = ['0 to 4 years', '5 to 9 years', '0 to 9 years']
+column_label = ['0 to 4 years', '5 to 9 years', '0 to 9 years']
 vmax = results[columns].max().max()
 
 
