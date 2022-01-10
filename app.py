1c5947a85110af3bf55cf63cd1a70e78de8d117b
surchs <sebastian.urchs@gmail.com>
2022-01-10 15:51:46 -0500

    adding basic table

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
+from dash import dash_table
+from dash.dash_table import Format
 import geopandas as gpd
 import pandas as pd
 import plotly.express as px
 from dash import html, dcc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, "assets/styles.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -12,10 +14,24 @@ app.title = "Home bilingualism in Canada"
 server = app.server
 
 results = pd.read_csv('assets/bilingual_results_with_region_codes.tsv', sep='\t')
+language_pairs = pd.read_csv('assets/bilingual_language_pairs.csv')
 geo_table = gpd.read_file('assets/recombined_shape_files.zip')
 columns = [col_name for col_name in results.columns if 'Percent' in col_name]
 vmax = results[columns].max().max()
 
+# Merge bilingual pairs and region results
+biling_index = [area in language_pairs.area.unique() for area in results.area.values]
+_res_biling = results[biling_index][['area', 'Region', 'name']]
+language_table = pd.merge(language_pairs, _res_biling, on='area')
+language_columns = [('language_pair', 'language_pair_collapsed'),
+                    ('% bil 0-4', 'percent_bilingual_children_age_0_to_4'),
+                    ('% bil 5-9', 'percent_bilingual_children_age_5_to_9'),
+                     ('% bil 0-9', 'percent_bilingual_children_age_0_to_9'),
+                      ('% all 0-4', 'percent_all_children_age_0_to_4'),
+                       ('% all 5-9', 'percent_all_children_age_5_to_9'),
+                        ('% all 0-9', 'percent_all_children_age_0_to_9')
+                    ]
+lang_columns = [{"name": name, "id": idx} for name, idx in language_columns]
 
 def make_figure(overlay=None):
     if overlay is None:
@@ -37,9 +53,9 @@ def make_figure(overlay=None):
                                            'Region': False
                                            },
                                labels={'province': 'Province',
-                                       'Percent_age_0_to_4': 'Home Bilingualism among children aged 0-4',
-                                       'Percent_age_5_to_9': 'Home Bilingualism among children aged 5-9',
-                                       'Percent_age_0_to_9': 'Home Bilingualism among children aged 0-9'},
+                                       'Percent_age_0_to_4': 'Home Bilingualism<br>among children aged 0-4',
+                                       'Percent_age_5_to_9': 'Home Bilingualism<br>among children aged 5-9',
+                                       'Percent_age_0_to_9': 'Home Bilingualism<br>among children aged 0-9'},
                                )
     fig['layout'].update(margin=dict(l=0, r=0, b=0, t=30))
     return fig
@@ -126,6 +142,32 @@ figure_card = dbc.Card(
     ]
 )
 
+table_card = dbc.Card(
+    [
+        dbc.CardHeader('This is a table'),
+        dbc.CardBody(
+            [
+                dbc.Row([
+                    'In the City of:',
+                    html.Div(id='city_name')
+                ]
+                ),
+            dbc.Row(dash_table.DataTable(
+                            id="table-lang",
+                            columns=lang_columns,
+                            data=language_table.query('Region==0').to_dict("records"),
+                            filter_action="native",
+                            style_table={"overflowY": "scroll"},
+                            fixed_rows={"headers": False, "data": 0},
+                            style_cell={"width": "85px"},
+                        ),
+            ),
+                ]
+        ),
+        dbc.CardFooter('Here is the footer')
+    ]
+)
+
 abstract_card = dbc.Card(
     [
 
@@ -149,7 +191,11 @@ app.layout = html.Div(
     [
         header,
         dbc.Container([
-            dbc.Row(dbc.Col(figure_card, md=8), justify='center'),
+            dbc.Row(
+                [
+                    dbc.Col(figure_card, md=8),
+                    dbc.Col(table_card, md=4)
+], justify='center'),
             dbc.Row(dbc.Col(abstract_card, md=8), justify='center')
         ], fluid=True)
     ]
@@ -162,6 +208,20 @@ app.layout = html.Div(
 )
 def update_overlay(selected_overlay):
     return make_figure(selected_overlay)
+
+
+@app.callback(
+    [Output('table-lang', 'data'), Output('city_name', 'children')],
+    Input("graph", "hoverData")
+)
+def update_table(hover):
+    if hover is None:
+        return dash.no_update, 'Nowhere'
+    hover_location = hover['points'][0]['location']
+    if not hover_location in language_table.Region.values:
+        return dash.no_update, dash.no_update
+    subset = language_table.query('Region == @hover_location')
+    return subset.to_dict("records"), hover['points'][0]['hovertext']
 
 
 if __name__ == "__main__":
